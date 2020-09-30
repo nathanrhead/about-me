bcca8b7e19170d3bd06bc7bb2214fc312cb7a9b6
Nathan Rhead Cox <nrc4@hotmail.com>
2020-09-29 17:55:21 -0700

    Completed the html text content with preliminary tagging. Commented out template code in style.css and app.js.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-'use strict'
+'use strict';
+
+// var likesTheBoys = prompt('do you like The Boys?');
+
+// if(likesTheBoys.toLowerCase() === 'yes' || likesTheBoys.toLowerCase() === 'y'){
+//   alert('I like that show too');
+// } else {
+//   alert('you are missing out');
+// }
+
