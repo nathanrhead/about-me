d93a12af9e8117199d5a4e2b3c9bacc19186fa91
Nathan Rhead Cox <nrc4@hotmail.com>
2020-09-29 22:51:23 -0700

    Finalized the app.js--it's not working entierly correctly, because I can't figure out how to store one variable at the beginning for use at the end--and the readme.md.

--- README.md
+++ README.md
@@ -28,14 +28,24 @@ User's story 2: As a user, I would like a personalized welcome message so that I
 
 * [submission PR](https://nathanrhead.github.io/about-me/)
 <!-- * Any Links you used as reference -->
+
 * [to reinstate bullets after the CSS reset](https://stackoverflow.com/questions/55970383/how-do-i-reinstate-list-style-type-bullets-following-a-css-reset)
-* 
+
+* [to genereate a random number between 0 and 10](https://medium.com/@josephcardillo/using-math-random-in-javascript-c49eff920b11)
 
 ### Reflections and Comments
 
 <!-- * Consider including the answers to your daily journal and submission questions here
 * This is also a good place to reflect on the tools and resources used and learned -->
 
+* Resetting the CSS doesn't make a ton of sense to me, yet, especially given that it means I have to code for bullet points on LIs that were originally hard-coded.
 
+* While single quotes may be the standard for JS, when you need to use an apostrophe in a string, you have to use double quotes to set off your string. [I’ve since learned about escaping the apostrophe with a backslash (e.g., Nathan\’s), but I find that approach rather unsightly. Duckett says you can just use double quotes, but CF says you can’t. 
+
+* Inline styling is confusing and prone to human error. 
+
+* I still don’t wholly understand the point of erasing all of the default styling in the browser, especially if we’re supposed to style everything with CSS anyway; it still seems like an superfluous step, especially if it takes out things like bullet points from listed items `<li>`
+
+* I was proud that I figured out, roughly, how to generate an even number.
 
 ### License ©nathanrhead
--- js/app.js
+++ js/app.js
@@ -1,10 +1,70 @@
 'use strict';
 
-// var likesTheBoys = prompt('do you like The Boys?');
+var welcome = prompt('May I ask your name?');
 
-// if(likesTheBoys.toLowerCase() === 'yes' || likesTheBoys.toLowerCase() === 'y'){
-//   alert('I like that show too');
-// } else {
-//   alert('you are missing out');
-// }
+    if(welcome.toUpperCase() === 'Yes' || welcome.toUpperCase() === 'YES' || welcome.toUpperCase() === 'Y') {
+        prompt('Great, type it here.'); //how do I store this second prompt?
+                
+    } else if(welcome.toLowerCase() === 'no' || welcome.toLowerCase() === 'n' || welcome.toLowerCase() === 'nope') {
+        alert('No worries, we\'ll just call you Pat.');
+        
+    } else if(typeof welcome === 'string' || typeof welcome === !null) {
+        alert(welcome + "," + " welcome to my brief bio page.");
+}
 
+var gameInvite = 'Yes'
+
+for(var i = 0; i < 10; i++) {
+    var gameInviteCorrect = prompt('Before I tell you a little bit about myself, would you like me to read your mind?');
+        console.log('The user\'s answer is ' + gameInviteCorrect);
+    
+    if(gameInviteCorrect.toLowerCase() === 'yes' || gameInviteCorrect.toLowerCase() === 'y' || gameInviteCorrect.toLowerCase() === 'yep') {
+        alert('Alright, pick any positive whole number, just don\'t tell me what it is.');
+        break; 
+    } else {
+    alert('Wrong answer.');
+    }
+}
+var gameRuleOne = 'Yes'
+
+for(var i = 0; i < 10; i++) {
+    var gameRuleOneAnswer = prompt('Got your positive whole number?'); 
+    if(gameRuleOneAnswer.toLowerCase() === 'yes' || gameRuleOneAnswer.toLowerCase() === 'y' || gameRuleOneAnswer.toLowerCase() === 'yep') {
+        alert('Perfect, let\'s get started!');
+        break;
+    } else if(gameRuleOneAnswer.toLowerCase() === 'no' || gameRuleOneAnswer.toLowerCase() === 'n' || gameRuleOneAnswer.toLowerCase() === 'nope') {
+        alert('No worries, I can wait.');
+    }
+}
+var gameRuleOnePointOne = alert('Now double it in your head.');
+
+var getRandomEvenNumber = (Math.ceil(Math.random() * 10 + 1) * 2);
+alert('Add ' + (getRandomEvenNumber) + ' to your new number.');
+
+var gameRuleTwo = alert('Now divide that new number by 2.');
+
+var gameRuleLast = alert('Subtract the number you started with from the number you have now.');
+
+var gameConclusion = alert('If you\'ve done the math right, your number is [drum roll, please]. . . .');
+
+var moreSuspense = alert('. . . [building suspense] . . .');
+
+var gameAnswer = alert('. . . your number is ' + (getRandomEvenNumber / 2) + '!');
+
+var loadPage = welcome;
+
+    if(welcome.toLowerCase() === 'no' || welcome.toLowerCase() === 'n' || welcome.toLowerCase() === 'nope') {
+    loadPage = alert('Thanks for playing, Pat! Did you enjoy the game?');
+
+    } else if(welcome.toLowerCase() === 'yes' || welcome.toLowerCase() === 'y' || welcome.toLowerCase() === 'yep' || welcome.toLowerCase() === 'string') {
+    loadPage = alert('Awesome, ' + welcome + '! Let\'s get better acquainted.'); //this isn't working
+}
+
+// prompt('Thanks for playing, ' + userName + ', did you enjoy the game?');
+
+// 1. Pick a number		(e.g. 10)
+// 2. Double it		20
+// 3. Add 44 (or any even number) to it		64
+// 4. Divide that number by 2		32
+// 4. Subtract the number you started with from the number you have now		22
+// 5. Your number is half of the number added in step 3		
